15fe239bd9ea06f408670aebedba4ad7998b45b8
Ivan Khanevich <ivan.khanevich@gmail.com>
2021-10-10 17:08:37 +0300

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Domainer
+Application to test [Cloudflare Pages](https://pages.cloudflare.com/) and [Cloudflare Workers](https://workers.cloudflare.com/). As base was used Svelte with Vite bundler, for styling - WindiCSS
---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Home from "./pages/index.svelte";
+</script>
+
+<header><h1>Domainer</h1></header>
+<main>
+  <Home />
+</main>
+<footer>
+  <small
+    >Created by
+    <a href="https://www.8byte.agency" target="_blank" rel="noopener noreferrer"
+      >8byte Agency</a
+    ></small
+  >
+</footer>
+
+<style>
+</style>
